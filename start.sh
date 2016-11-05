e6ee6e64f6a03d236852a991af71ffa63690302d
Duc Anh Babim <ducanh.babim@yahoo.com>
2016-11-06 02:40:12 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Prepare
-if [ ! -f "/usr/local/MONyog/README" ]; then mv /usr-start/MONyog/* /usr/local/MONyog; fi
+if [ ! -f "/usr/local/MONyog/README" ]; then cp -R /usr-start/MONyog/* /usr/local/MONyog; fi
 
 /etc/init.d/MONyogd start
 bash
