9793ad1a2b6b36c0f8f0a6a3c8136dba4190cec5
Duc Anh Babim <ducanh.babim@yahoo.com>
2019-03-15 12:08:38 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# option with entrypoint
+if [ -f "/option.sh" ]; then /option.sh; fi
+
 # Prepare
 if [ ! -f "/usr/local/MONyog/README" ]; then cp -R /usr-start/MONyog/* /usr/local/MONyog; fi
 
